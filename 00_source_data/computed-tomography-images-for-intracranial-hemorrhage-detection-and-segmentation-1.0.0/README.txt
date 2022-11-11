4bdba52dd8692170614660df0163568b405c6bcb
abzdel <abzdel25@gmail.com>
2022-11-11 09:37:45 -0500

    added source data

---
+++ 00_source_data/computed-tomography-images-for-intracranial-hemorrhage-detection-and-segmentation-1.0.0/README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5727d329ba741d1ab3403c0b5cf1d554297206c74bb1813fc4641873a5181afd
+size 3022
---
+++ 00_source_data/computed-tomography-images-for-intracranial-hemorrhage-detection-and-segmentation-1.0.0/split_data.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1ef34f5310489dab0f04d837ca24e3050f5a4f1ccea1bab67c0805f492485dad
+size 2499
